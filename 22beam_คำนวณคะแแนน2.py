a981fcc635341fc395a52f3097837a1cfa468228
27196-cell <27196@bbw.ac.th>
2026-06-22 09:36:27 +0700

    Update 22beam_คำนวณคะแแนน2.py

--- 22beam_คำนวณคะแแนน2.py
+++ 22beam_คำนวณคะแแนน2.py
@@ -1,4 +1,4 @@
-print("โปรแกรมคำนวณคะแนน")
+print("โปรแกรมคำนวณคะแนน\n")
 
 Eng = int(input("ระบุคะแนนวิชาภาษาอังกฤษ: "))
 Math = int(input("ระบุคะแนนวิชาคณิตศาสตร์: "))
@@ -7,12 +7,12 @@ Chemistry = int(input("ระบุคะแนนวิชาเคมี: "))
 total_point = Eng + Math + Chemistry
 average = int(total_point /3)
 
-print("คะแนนรวม:",total_point)
-print("คะแนนเฉลี่ย:", average)
+print("คะแนนรวม:",total_point , "คะแนน")
+print("\nคะแนนเฉลี่ย:", average , "คะแนน")
 if average < 60:
     print("ควรปรับปรุง")
 elif average < 80:
     print("ผ่าน")
 else:
     print("ดีเยี่ยม")
-print("made by Borinot Maiaiem Beam No.22")
+print("\nmade by Borinot Maiaiem Beam No.22")
